f47089eadc014308056d6948b24391bd3bb8baec
ahrobinson <ahrobins3@gmail.com>
2015-10-19 23:37:00 -0500

    user can create and enter rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,41 @@
 // YOUR CODE HERE:
 $(document).ready(function(){
-  $('.submit').on('click', function(e){
+
+  $('#send').on('submit', function(e){
     // debugger;
     e.preventDefault();
     console.log('button clicked');
     app.handleSubmit();
   });
+  //use this because rooms are being dynamically added
   $(document.body).on('change', '#roomSelect', function(e) {
     // e.preventDefault();
+    if ($(this).val() === 'New Room') {
+      var room = prompt("What's the name of your room?");
+      app.addRoom(escapeHtml(room));
+      console.log(escapeHtml(room));
+      $(this).val(escapeHtml(room));
+    }
+
     app.roomName = $(this).val();
-    app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&roomname=' + app.roomName);
+
+    // var where = encodeURIComponent('where={"roomname":' + app.roomName +'}');
+
+    app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&where%3D%7B%22roomname%22%3A%22' + escapeHtml(app.roomName) + '%22%7D');
+
+    // app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&' + where);
+
   });
 
 });
+
+
 var app = {
   init: function(messages) {
     // var messages = this.fetch('https://api.parse.com/1/classes/chatterbox');
     // console.log(messages)
+
+    //grabs the username out of url parameter
     function GetURLParameter(sParam){
       var sPageURL = window.location.search.substring(1);
       var sURLVariables = sPageURL.split('&');
@@ -27,10 +46,14 @@ var app = {
         }
       }
     }
-
+    //sets userName to users name
     app.userName = GetURLParameter('username');
-    console.log(app.userName);
-    app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&roomname=' + app.roomName);
+    // console.log(app.userName);
+    app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000');
+
+    // setInterval(function(){
+    //   app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&where%3D%7B%22roomname%22%3A%22' + escapeHtml(app.roomName) + '%22%7D');
+    // }, 3000);
   },
   send: function(message){
     $.ajax({
@@ -42,7 +65,8 @@ var app = {
       success: function (data) {
         console.log('chatterbox: Message sent. Data: ', data);
         // Show something on the page to user that message sent.
-        app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&roomname=' + app.roomName);
+        //grabs posts in descending order
+        app.fetch('https://api.parse.com/1/classes/chatterbox?order=-createdAt&limit=1000&where%3D%7B%22roomname%22%3A%22' + escapeHtml(app.roomName) + '%22%7D');
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -71,6 +95,7 @@ var app = {
   refreshView: function(messages){
     var a = messages.map(function(content){
       if(app.rooms.indexOf(content.roomname) === -1){
+        //add rooms to room array
         app.rooms.push(content.roomname);
       }
       var $div = $('<div class="content">');
@@ -86,6 +111,8 @@ var app = {
       $select.append('<option value="' + escapeHtml(app.rooms[i]) +'">'+ escapeHtml(app.rooms[i]) + '</option>');
     };
     $('.roomList').append($select);
+    //this makes dropdown menu stay on selected option
+    $('#roomSelect').val(app.roomName);
   },
   clearMessages: function(){
 
@@ -98,10 +125,13 @@ var app = {
     app.send(message);
   },
   addRoom: function(roomname){
+    room = { name: roomname };
     app.rooms.push(roomname);
     var $option = $('<option value="' + roomname + '">');
     $option.html(roomname);
     $('#roomSelect').append($option);
+    app.roomName = roomname;
+
   },
   addFriend: function(){
 
@@ -109,20 +139,22 @@ var app = {
   handleSubmit: function(){
 
     var text = $('input').val().trim();
-    var roomName = 'HRR9';
+    var roomName = app.roomName;
     var message = new Message(app.userName, text, roomName);
     app.send(message);
   },
   userName: undefined,
   rooms: [],
-  roomName: undefined,
+  friends: [],
+  roomName: 'HRR9',
 };
+//creates new message
 var Message = function(username, text, roomName){
   this.username = username;
   this.text = text;
   this.roomname = roomName;
 };
-
+// escpaping function
 var entityMap = {
     "&": "&amp;",
     "<": "&lt;",
@@ -138,4 +170,20 @@ var escapeHtml = function (string) {
     return entityMap[s];
   });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
